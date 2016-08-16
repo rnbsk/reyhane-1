60f218e920624cc009d8a9605ddea2163c85d4fb
rnbsk <r.nobakhti.sk@gmail.com>
2016-08-15 21:41:54 -0700

    new file
    یک برنامه ی جدید

---
+++ Git.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author pc_
+ */
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    Git(){
+        Git git = new Git();
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Salam");
+        System.out.println("apple");
+    }
+    
+}
